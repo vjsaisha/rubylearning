29980061282472afd98f3473331ed57d433d9edc
vjsaisha <vjsaisha@gmail.com>
2018-08-15 11:12:19 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,12 +1,2 @@
-import unittest
-
-import awesome
-
-
-class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(awesome.smile(), ":)")
-
-
-if __name__ == '__main__':
-    unittest.main()
+print("Hello World")
+print("vijay")
